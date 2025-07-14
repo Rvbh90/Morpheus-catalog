a698f2d0a5ff6fe6ecf86f2a14a65a05725e76b9
Rakesh <94463803+Rvbh90@users.noreply.github.com>
2025-07-14 12:50:18 -0500

    Update integration.py

--- setup/integration.py
+++ setup/integration.py
@@ -2964,7 +2964,7 @@ def get_integration_id(api_base, headers, int_name):
         data = response.json()
         integrations = data.get("integrations", [])
         if integrations:
-            REPO_ID = integrations[0].get("id")
+            REPO_ID = integrations[0].get("id")-1
             print(f"Integration ID for '{int_name}': {REPO_ID}")
             return REPO_ID
         else:
